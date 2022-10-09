f3d72d0fd579cab217323d3019b38e400c09fedf
YeosCRN <jeoscrn20@outlook.com>
2022-10-09 15:44:46 -0700

    print6

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 	
-	/**
-	 * main - command to print a sentence
-	 * return: program must end with the value 0	
-	 */
+/**
+ * main - command to print a sentence
+ * return: program must end with the value 0	
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

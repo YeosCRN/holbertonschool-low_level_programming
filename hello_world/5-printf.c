bcc40a1e12f81c23e66877fa7dd13609687ba5b5
YeosCRN <jeoscrn20@outlook.com>
2022-10-09 16:23:33 -0700

    printfwithC

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+include <stdio.h>
+
+/**
+ * main - prints a sentence ending with new line
+ * Return: value must end in 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

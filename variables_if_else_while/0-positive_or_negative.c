6a0b9fc1dc194dad9840eee8cf67555cdf395dcf
YeosCRN <jeoscrn20@outlook.com>
2022-10-10 17:18:09 -0500

    if-else5

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- *main - program will be able to determite if random generated number is positive, negative or zero
+ * main - program to designate number nature
  */
 int main(void)
 {

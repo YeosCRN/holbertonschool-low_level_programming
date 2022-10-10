cc4d8738c6cfd00f338e1d57513deaed84bfabef
YeosCRN <jeoscrn20@outlook.com>
2022-10-10 17:10:30 -0500

    if-else3

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
     if (n < 0)
-    printf("%d is negative");
+    printf("%d is negative", n);
    	else if (n > 0)
-    printf("%d is positive");
+    printf("%d is positive", n);
     else
-    printf("%d is zero");
+    printf("%d is zero", n);
     scanf("%d", &n);
 	return (0);
 }

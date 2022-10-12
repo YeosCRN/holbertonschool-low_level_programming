792caf2449ff977d596725244d356054a0170f6a
YeosCRN <jeoscrn20@outlook.com>
2022-10-12 11:34:30 -0500

    0to9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ * Return: value must return to 0
+ */
+int main(void)
+{
+	int x;
+	for (x = 0; x < 10; x++)
+	{
+	printf ("%d", x);
+	}
+	printf ("\n", x);
+	return (0);
+}

eb11b8b2a692ade63fd42c3bfdb302ce514b3ac4
YeosCRN <jeoscrn20@outlook.com>
2022-10-12 11:06:23 -0500

    alphabts

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -2,18 +2,18 @@
 
 /**
  * main - print alphabts
- *  Return: value must return to 0
+ * Return: value must return to 0
  */
 int main(void)
 {
-    char x = 'a';
+	char x = 'a';
 
-    while (x <= 'z')
-    {
-        if (x != 'e' && x != 'q')
-        putchar(x);
-        x++;
-    }
-    putchar('\n');
-    return (0);
+	while (x <= 'z')
+	{
+		if (x != 'e' && x != 'q')
+			putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
 }

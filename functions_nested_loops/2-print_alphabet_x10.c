84ea70daef8b0718d8fca13cee22849375c883f4
YeosCRN <jeoscrn20@outlook.com>
2022-10-12 19:54:52 -0500

    10times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet x10
+ */
+void print_alphabet_x10(void)
+{
+	char x, y = 0;
+
+	while (y < 10)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+		_putchar(x);
+		}
+	_putchar('\n');
+	y++;
+	}
+}

5e70dee2b952f4688eef507ad8e0d8b82adc2555
YeosCRN <jeoscrn20@outlook.com>
2022-10-14 17:25:28 -0500

    ya casi

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints square
+ * @size: int
+ */
+void print_square(int size)
+{
+	char x , y;
+
+	if (size > 0)
+	{
+	for (x = 1; x <= size; x++)
+	{
+		for (y = 1; y <= size; y++)
+		{
+			_putchar('#');
+		}
+		_putchar(10);
+	}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}

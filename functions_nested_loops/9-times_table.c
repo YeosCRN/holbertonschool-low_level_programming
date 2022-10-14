e510fb3a5c546d22645af3f2652e0fb612826346
YeosCRN <jeoscrn20@outlook.com>
2022-10-13 19:42:18 -0500

    x9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * times_table - print 9 times table
+ */
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			_putchar((z / 10) + '0');
+			_putchar((z % 10) + '0');
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar(10);
+	}
+}

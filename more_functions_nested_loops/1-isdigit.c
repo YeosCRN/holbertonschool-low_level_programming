9afe7df924ecc1d5ef19d959d489c60d4c115bba
YeosCRN <jeoscrn20@outlook.com>
2022-10-14 15:45:01 -0500

    digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check for digit
+ * @c: int
+ * Return: 1 digit
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

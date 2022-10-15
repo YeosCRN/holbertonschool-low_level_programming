01c8d7edad85f297a8ce2bb7d58e113b6ab8ecb3
YeosCRN <jeoscrn20@outlook.com>
2022-10-14 20:08:39 -0500

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_square - prints square
+ * @size: int
+ */
+void print_triangle(int size)
+{
+	char x, y;
+
+	if (size > 0)
+	{
+	for (x = 1; x <= size; x++)
+	{
+		for (y = 1; y <= size; y++)
+		{
+			if ((size - x) < y)
+			{
+				_putchar('#');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar(10);
+	}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}

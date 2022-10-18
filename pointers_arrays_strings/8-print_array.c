faaa4a3dd3d7fbffed96e9db605302d7a8b15c67
YeosCRN <jeoscrn20@outlook.com>
2022-10-18 18:38:23 -0500

    octava

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * puts_half - code
- * @str: pointer
+ * print_array - code
+ * @a: pointer
+ * @n: var
  */
 void print_array(int *a, int n)
 {
-    int x = 0;
-    for (x = 0; x <= n - 1; x++)
-    {
-            printf("%d", a[x]);
-        if (x != n - 1)
-        {
-            printf(", ");
-        }
-    }
-    printf("\n");
+	int x = 0;
+	for (x = 0; x <= n - 1; x++)
+	{
+			printf("%d", a[x]);
+		if (x != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }

efd3289d3119bd38068b6ab9f539f4d1c8fae736
YeosCRN <jeoscrn20@outlook.com>
2022-10-18 18:49:54 -0500

    novena

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - code
+ * @dest: pointer
+ * @src: pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return dest;
+}

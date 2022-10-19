caccd963de1709d4516682895583f93a2c5b2572
YeosCRN <jeoscrn20@outlook.com>
2022-10-19 13:31:28 -0500

    strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - code
+ * @dest: pointer
+ * @src: pointer
+ * @n: var
+ * Return: dest
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int x, y, z;
+
+	y = strlen(s1);
+	x = 0;
+	while (x < y)
+	{
+		if (s1[x] != s2[x])
+		{
+			return (s1[x] - s2[x]);
+			break;
+		}
+		x++;
+		if (x == '\0')
+			break;
+			return (0);
+			
+		
+	}
+}

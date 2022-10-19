d0ee6c0095cb023b021debce1ec4ab64593c137c
YeosCRN <jeoscrn20@outlook.com>
2022-10-19 14:22:47 -0500

    rev-array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -7,24 +7,13 @@
  */
 void reverse_array(int *a, int n)
 {
-    int length, x;
-    int *begin, *end, temporal;
+    int x, temporal;
 
-    length = sizeof(a);
-    x = sizeof(int);
-    begin = a;
-    end = a;
-
-    for (n = 0; n < length + x; n++)
-        end++;
-
-    for (n = 0; n < length / 2; n++)
+    for (x = 0; x <= (n / 2); x++)
     {
-        temporal = *end;
-        *end = *begin;
-        *begin = temporal;
+        temporal = a[n - x - 1];
+        a[n - x - 1] = a[x];
+        a[x] = temporal;
 
-        begin++;
-        end--;
     }
 }
---
+++ pointers_arrays_strings/copy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - code
+ * @a: pointer
+ * @n: var
+ */
+void reverse_array(int *a, int n)
+{
+	int length, x;
+	int *begin, *end, temporal;
+
+	length = sizeof(a);
+	x = sizeof(int);
+	begin = a;
+	end = a;
+
+	for (n = 0; n < length + x; n++)
+		end++;
+
+	for (n = 0; n < length / 2; n++)
+	{
+		temporal = *end;
+		*end = *begin;
+		*begin = temporal;
+
+		begin++;
+		end--;
+	}
+}

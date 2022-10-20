628bc1ccad79381c52b05e27bb3d1fcbf92e9ae7
YeosCRN <jeoscrn20@outlook.com>
2022-10-20 10:48:54 -0500

    test

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,7 @@ char *cap_string(char *scr)
 		if (x == 0 && scr[x + 1] >= 'a' && scr[x + 1] <= 'z')
 			scr[x] -= 32;
 
-		for (z = 0; z < 12; z++)
+		for (z = 0; z < 13; z++)
 		{
 			if (scr[x] == p[z])
 			{

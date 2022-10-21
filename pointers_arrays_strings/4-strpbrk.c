fbf4e3692edeced3c52e19d9a355678ce6caf14d
YeosCRN <jeoscrn20@outlook.com>
2022-10-21 17:35:08 -0500

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - code
+ * @s: pointer
+ * @accept: pointer
+ * Return: s after finding str
+ */
+char *_strpbrk(char *s, char *accept)
+{
+    int x, y;
+
+    y = strlen(s);
+    x = 0;
+    while (x < y)
+    {
+        if (s[x] == accept[x] && s[x] != '\0')
+        {
+            return (s + x - 1);
+        }
+        x++;
+    }
+    if (accept[x] == '\0')
+    {
+        return (s + x - 1);
+    }
+    return (NULL);
+}

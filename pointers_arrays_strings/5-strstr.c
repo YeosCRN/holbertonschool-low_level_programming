dedc90097adc02dbd6212660bda81cadb35de93c
YeosCRN <jeoscrn20@outlook.com>
2022-10-21 18:21:54 -0500

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * _strpbrk - code
- * @s: pointer
- * @accept: pointer
- * Return: s after finding str
+ * _strstr - code
+ * @haystack: pointer
+ * @needle: pointer
+ * Return: haystack after finding str
  */
 char *_strstr(char *haystack, char *needle)
 {
-    int x, y;
+	int x, y;
 
-    x = 0;
-    while (haystack[x] != '\0')
-    {
-        y = 0;
-        while (needle[y] != '\0')
-        {
-            if (haystack[x] == needle[y] && haystack[x + 1] == needle[y + 1])
-            {
-                return (haystack + x);
-            }
-            y++;
-        }
-        x++;
-    }
-    return (NULL);
+	x = 0;
+	while (haystack[x] != '\0')
+	{
+		y = 0;
+		while (needle[y] != '\0')
+		{
+			if (haystack[x] == needle[y] && haystack[x + 1] == needle[y + 1])
+			{
+				return (haystack + x);
+			}
+			y++;
+		}
+		x++;
+	}
+	return (NULL);
 }

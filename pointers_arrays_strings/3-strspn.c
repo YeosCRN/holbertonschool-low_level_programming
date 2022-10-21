6467ab55190199777c0a14773d7a838bcdadb196
YeosCRN <jeoscrn20@outlook.com>
2022-10-21 16:28:32 -0500

    strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strspn - code
+ * @s: pointer
+ * @accept: pointer
+ * Return: dest
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+    int x, y;
+
+    x = strlen(s);
+    y = strlen(accept);
+    return (y + 1);
+}

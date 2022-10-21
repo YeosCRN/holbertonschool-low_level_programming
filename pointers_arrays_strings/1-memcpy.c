f3b0135286c9df5639d6add85dc141d4456ff3a4
YeosCRN <jeoscrn20@outlook.com>
2022-10-21 15:14:33 -0500

    memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _memcpy - code
+ * @dest: pointer
+ * @src: pointer
+ * @n: var
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	{
+		unsigned int x, y;
+
+		y = strlen(dest);
+		for (x = 0; x < n; x++)
+		{
+			dest[y + x] = src[x];
+			if (src[x] == '\0')
+			{
+				break;
+			}
+		}
+
+		return (dest);
+	}
+}

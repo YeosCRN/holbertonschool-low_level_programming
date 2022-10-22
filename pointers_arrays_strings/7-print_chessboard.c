73742fe4603c8e793ee080a4cc660301532143a6
YeosCRN <jeoscrn20@outlook.com>
2022-10-21 20:09:00 -0500

    chess

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - code
+ * @a: array
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, y, colunm;
+
+	colunm = sizeof(a[0])/sizeof(a[0][0]);
+	for (x = 0; x < 8; x++)
+	{
+		for(y = 0; y < colunm; y++)
+		{
+			printf("%c", a[x][y]);
+		}
+		printf("\n");
+	}
+}

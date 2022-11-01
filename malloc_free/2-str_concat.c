d9a1a7714b3f8bbd717495a01149709670442ab7
YeosCRN <jeoscrn20@outlook.com>
2022-11-01 11:39:56 -0500

    str_concat

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * str_concat - function
+ * @s1: str
+ * @s2: str
+ * Return: pointer, NULL for fail
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int x, y, z, total;
+	char *c;
+
+	for (y = 0; s1[y] != '\0'; y++)
+	{
+	}
+	for (z = 0; s2[z] != '\0'; z++)
+	{
+	}
+	c = malloc(sizeof(char) * (y + z + 1));
+
+	for (x = 0; s1[x] != '\0'; x++)
+		c[x] = s1[x];
+
+	for (total = 0; s2[total] != '\0'; total++)
+		c[total + y] = s2[total];
+
+	return (c);
+}

bf35743bc01649c107b13b03722d7a01015706a0
YeosCRN <jeoscrn20@outlook.com>
2022-11-01 16:21:14 -0500

    free_grid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -3,12 +3,11 @@
  * free_grid - function
  * @grid: 2D array
  * @height: var
- * Return: pointer, NULL for fail
  */
 void free_grid(int **grid, int height)
 {
-	int x; 
-	
+	int x;
+
 	for (x = 0; x < height; x++)
 	{
 		free(grid[x]);

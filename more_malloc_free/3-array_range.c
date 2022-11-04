6c1d910ae51904da5dc4851b1762f40a8319da24
YeosCRN <jeoscrn20@outlook.com>
2022-11-04 15:50:52 -0500

    range

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,26 +1,26 @@
 #include "main.h"
 /**
- * create_array - function
- * @size: var
- * @c: var
- * Return: pointer, NULL for fail
+ * array_range - function
+ * @min: var
+ * @max: var
+ * Return: array, NULL for fail
  */
 int *array_range(int min, int max)
 {
-    int x, mid;
-    int *a;
+	int x, mid;
+	int *a;
 
-    if (min > max)
-        return (NULL);
+	if (min > max)
+		return (NULL);
 
-    mid = max - min + 1;
+	mid = max - min + 1;
 
-    a = malloc(sizeof(int) * mid);
-    if (a == NULL)
-        return (NULL);
+	a = malloc(sizeof(int) * mid);
+	if (a == NULL)
+		return (NULL);
 
-    for (x = 0; x < mid; x++)
-        a[x] = min++;
+	for (x = 0; x < mid; x++)
+		a[x] = min++;
 
-    return (a);
+	return (a);
 }

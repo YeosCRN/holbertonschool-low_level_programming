089095d1107709173b9ba7225827c249b8d65c50
YeosCRN <jeoscrn20@outlook.com>
2022-11-11 10:56:04 -0500

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef CAL_H
+#define CAL_H
+
+/**
+ * struct dog - Struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif

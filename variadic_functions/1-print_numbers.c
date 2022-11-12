b2b1ae30438b3e82ef4ec9412031d1d293917c2e
YeosCRN <jeoscrn20@outlook.com>
2022-11-11 21:17:16 -0500

    print_numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -6,24 +6,24 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list numbers;
-    unsigned int x;
+	va_list numbers;
+	unsigned int x;
 
-    va_start(numbers, n);
+	va_start(numbers, n);
 
-    for (x = 0; x < n; x++)
-    {
-        if (separator != NULL && x < (n - 1))
-        {
-            printf("%d%s", va_arg(numbers, int), separator);
-        }
+	for (x = 0; x < n; x++)
+	{
+		if (separator != NULL && x < (n - 1))
+		{
+			printf("%d%s", va_arg(numbers, int), separator);
+		}
 
-        else
-        {
-            printf("%d", va_arg(numbers, int));
-        }
-    }
-    va_end(numbers);
+		else
+		{
+			printf("%d", va_arg(numbers, int));
+		}
+	}
+	va_end(numbers);
 
-    printf("\n");
+	printf("\n");
 }

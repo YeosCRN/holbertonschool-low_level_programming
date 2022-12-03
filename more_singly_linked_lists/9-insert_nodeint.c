6fa11b447120455ef2a21840fe4dda975eebb8b0
YeosCRN <YeosCRN@jeoscrn20@outlook.com>
2022-12-02 21:56:49 -0500

    final2

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 	dpc->n = n;
 	if (index == 0)
 	{
-		new->next = current;
+		dpc->next = current;
 		*head = dpc;
 		return (*head);
 	}

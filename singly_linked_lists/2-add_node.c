e9f2a7437afb4f089243557e9ad0d040c0c53a04
YeosCRN <YeosCRN@jeoscrn20@outlook.com>
2022-12-02 20:58:05 -0500

    final3

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,25 +7,25 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    char *bck;
-    int length;
-    list_t *x;
+	char *bck;
+	int length;
+	list_t *x;
 
-    x = (list_t *)malloc(sizeof(list_t));
-    if (x == NULL)
-    {
-        return (NULL);
-    }
-    length = strlen(str);
-    bck = strdup(str);
-    if (bck == NULL)
-    {
-        free(x);
-        return (NULL);
-    }
-    x->str = bck;
-    x->len = length;
-    x->next = *head;
-    *head = x;
-    return (x);
+	x = (list_t *)malloc(sizeof(list_t));
+	if (x == NULL)
+	{
+		return (NULL);
+	}
+	length = strlen(str);
+	bck = strdup(str);
+	if (bck == NULL)
+	{
+		free(x);
+		return (NULL);
+	}
+	x->str = bck;
+	x->len = length;
+	x->next = *head;
+	*head = x;
+	return (x);
 }

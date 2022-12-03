c20f9ce9de6412ba8bf53eed1ba91cb35f2bdcaa
YeosCRN <YeosCRN@jeoscrn20@outlook.com>
2022-12-02 20:57:05 -0500

    final3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,26 +9,26 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *x, *y;
-    char *bck;
+	list_t *x, *y;
+	char *bck;
 
-    bck = strdup(str);
-    if (bck == NULL)
-        return (NULL);
-    x = *head;
-    while (x && x->next)
-        x = x->next;
-    y = (list_t *)malloc(sizeof(list_t));
-    if (y == NULL)
-        return (NULL);
-    if (x == NULL)
-    {
-        x = y;
-        *head = y;
-    }
-    x->next = y;
-    y->len = strlen(str);
-    y->str = bck;
-    y->next = NULL;
-    return (y);
+	bck = strdup(str);
+	if (bck == NULL)
+		return (NULL);
+	x = *head;
+	while (x && x->next)
+		x = x->next;
+	y = (list_t *)malloc(sizeof(list_t));
+	if (y == NULL)
+		return (NULL);
+	if (x == NULL)
+	{
+		x = y;
+		*head = y;
+	}
+	x->next = y;
+	y->len = strlen(str);
+	y->str = bck;
+	y->next = NULL;
+	return (y);
 }

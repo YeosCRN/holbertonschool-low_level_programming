12eed14b7cc2b0a29a3aba1b09b89e61bb84c9fd
YeosCRN <YeosCRN@jeoscrn20@outlook.com>
2022-12-09 21:03:27 -0500

    5doublyfinal

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - code
+ * @head: pointer
+ * @index: var
+ * Return: node, NULL if fail
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    dlistint_t *temp;
+    unsigned int i = 0;
+
+    if (head == NULL)
+        return (NULL);
+
+    temp = head;
+    while (temp)
+    {
+        if (i == index)
+            return (temp);
+        temp = temp->next;
+        i++;
+    }
+    return (NULL);
+}

6bd8dbfb7f77dc14ab3674f343af29d9c1c6af84
YeosCRN <YeosCRN@jeoscrn20@outlook.com>
2022-12-09 20:56:40 -0500

    1doublyfinal

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - code
+ * @h: pointer
+ * Return: count
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+    int count = 0;
+
+    while (h)
+    {
+        count++;
+        h = h->next;
+    }
+    return (count);
+}

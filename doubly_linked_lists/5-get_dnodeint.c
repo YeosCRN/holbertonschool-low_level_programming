bdd968ff9c05f31d07d1f2112e330c410e51449c
YeosCRN <YeosCRN@jeoscrn20@outlook.com>
2022-12-09 21:04:28 -0500

    5doublyfinal2

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -9,19 +9,19 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    dlistint_t *temp;
-    unsigned int i = 0;
+	dlistint_t *temp;
+	unsigned int i = 0;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    temp = head;
-    while (temp)
-    {
-        if (i == index)
-            return (temp);
-        temp = temp->next;
-        i++;
-    }
-    return (NULL);
+	temp = head;
+	while (temp)
+	{
+		if (i == index)
+			return (temp);
+		temp = temp->next;
+		i++;
+	}
+	return (NULL);
 }
